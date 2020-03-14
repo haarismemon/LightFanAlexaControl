fd29f213b68e00ba755c65002507508a391cfc03
haarismemon <memonhaaris97@gmail.com>
2020-03-14 20:14:22 +0000

    Refactor the actions into separate methods

--- LightFanControl.py
+++ LightFanControl.py
@@ -10,9 +10,11 @@ import time
 app = Flask(__name__)
 ask = Ask(app, '/')
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+
 ir_ctl = "/usr/bin/ir-ctl"
 directory = "/home/pi/Light-Fan-Control/IR_Codes/"
 device = "--device=/dev/lirc0"
+send_command = "--send=" + directory
 
 @ask.launch
 def launch():
@@ -26,140 +28,131 @@ def session_ended():
 
 @ask.intent('LightControlIntent', mapping={'light_status': 'light_status'})
 def lightControl(light_status):
-    ir_action = "--send=" + directory
-    
-    cmd = [ir_ctl, device]
-
-    print_message = ""
     
     if light_status in ["toggle", "double", "double switch", "repeat"]:
-        ir_action += "light_toggle"
+        do_double_light_toggle()
         
-        cmd.extend([ir_action] * 2)
-        
-        command_delay = "-g 250000" 
-        cmd.append(command_delay)
-        
-        print_message = """<speak>
-                            <audio src=\"soundbank://soundlibrary/musical/amzn_sfx_bell_short_chime_01\"/>
-                            <audio src=\"soundbank://soundlibrary/musical/amzn_sfx_bell_short_chime_02\"/>
-                            </speak>"""
+        return statement("""<speak>
+            <audio src=\"soundbank://soundlibrary/musical/amzn_sfx_bell_short_chime_01\"/>
+            <audio src=\"soundbank://soundlibrary/musical/amzn_sfx_bell_short_chime_02\"/>
+            </speak>""")
         
     elif light_status == "on":
-        ir_action += "light_toggle"
-        cmd.append(ir_action)
-        print_message = """<speak><audio src=\"soundbank://soundlibrary/musical/amzn_sfx_bell_short_chime_02\"/></speak>"""
+        do_light_toggle()
+            
+        return statement( """<speak><audio src=\"soundbank://soundlibrary/musical/amzn_sfx_bell_short_chime_01\"/></speak>""")
         
     elif light_status == "off":
-        ir_action += "light_toggle"
-        cmd.append(ir_action)
-        print_message = """<speak><audio src=\"soundbank://soundlibrary/musical/amzn_sfx_bell_short_chime_01\"/></speak>"""   
+        do_light_toggle()
+            
+        return statement("""<speak><audio src=\"soundbank://soundlibrary/musical/amzn_sfx_bell_short_chime_02\"/></speak>""")
         
     else:
-        print_message = "Did not understand: " + light_status 
-        
-        
-    print(cmd)
+        return statement("Did not understand: " + light_status)
 
-    subprocess.call(cmd)
-    
-    return statement(print_message)
 
 @ask.intent('DimControlIntent', mapping={'dim_percentage': 'dim_percentage', 'dim_status': 'dim_status'})
 def dimControl(dim_percentage, dim_status):
-    ir_action = "--send=" + directory
-    
-    cmd = [ir_ctl, device]
-
-    print_message = ""
     
     full = 25
     
     if dim_percentage == None:
-        
         if dim_status == "fade":
-            light_action = ir_action + "light_toggle"
-            
-            ir_action += "dim_cycle"
-            
-            # repeat in under to dim light continuously until very low
-            cmd.extend([ir_action] * full)
-            
-            # then turn off light
-            cmd.append(light_action)
-            
-            multi_command_delay = "-g 20000"    
-            
-            cmd.append(multi_command_delay)
-            
-            subprocess.call(cmd)
-            
+            do_light_dim(full, 0)
             time.sleep(0.5)
-            
-            light_cmd = [ir_ctl, device, light_action]
-            subprocess.call(light_cmd)
-            print light_cmd
-            
-            print_message = "Lights faded out"
+            do_light_toggle()
+            return statement("Lights faded out")
         
     else:     
-        ir_action += "dim_cycle"
-        
-        percent = 1 - (float(dim_percentage) / 100)
-        
-        setting = int(round(25 * percent))
-        
-        # repeat in under to dim light continuously
-        cmd.extend([ir_action] * setting)
-        
-        multi_command_delay = "-g 20000"    
-        
-        cmd.append(multi_command_delay)
-        
-        print_message = "Lights dimmed to " +  str(dim_percentage) + " percent"
-        
-        subprocess.call(cmd)
+        do_light_dim(full, dim_percentage)
+        return statement("Lights dimmed to " +  str(dim_percentage) + " percent")
     
-    print(cmd)
-    
-    return statement(print_message)
 
 @ask.intent('FanControlIntent', mapping={'fan_status': 'fan_status'})
-def fanControl(fan_status):    
-    ir_action = "--send=" + directory
-    
-    cmd = [ir_ctl, device]
-
-    print_message = ""
+def fanControl(fan_status):
     
     if fan_status == "off":
-        ir_action += "fan_off"
-        print_message = "Turning fan off"
+        do_fan_off()
+        return statement("Fan is off")
         
     elif fan_status == "low":
-        ir_action += "fan_low"
-        print_message = "Turning fan onto low setting"
+        do_fan_low()
+        return statement("Fan is on low")
         
     elif fan_status == "medium":
-        ir_action += "fan_medium"
-        print_message = "Turning fan onto medium setting"
+        do_fan_medium()
+        return statement("Fan is on medium")
         
     elif fan_status in ["high", "hi"]:
-        ir_action += "fan_high"
-        print_message = "Turning fan onto high setting"
+        do_fan_high()
+        return statement("Fan is on high")
         
     else:
-        print_message = "Did not understand: " + fan_status 
+        return statement("Did not understand: " + fan_status)
 
-    cmd.extend([ir_action] * 3)
-    multi_command_delay = "-g 20000"    
-    cmd.append(multi_command_delay)
+
+def do_light_toggle():
+    ir_action = send_command + "light_toggle"
+    cmd = [ir_ctl, ir_action]
     
-    print(cmd)
-
     subprocess.call(cmd)
+    print("performed command: " + str(cmd))
+
+def do_double_light_toggle():
+    ir_action = [send_command + "light_toggle"] * 2    
+    command_delay = "-g 250000" 
+    cmd = [ir_ctl, command_delay] + ir_action
     
-    return statement(print_message)
+    subprocess.call(cmd)
+    print("performed command: " + str(cmd))
+    
+
+def do_light_dim(full, dim_percentage):
+    percent = 1 - (float(dim_percentage) / 100)
+    setting = int(round(25 * percent))
+    
+    # repeat in order to dim light continuously
+    ir_action = [send_command + "dim_cycle"] * setting
+    multi_command_delay = "-g 20000"
+    
+    cmd = [ir_ctl, multi_command_delay] + ir_action
+    
+    subprocess.call(cmd)
+    print("performed command: " + str(cmd))
+
+
+def do_fan_off():
+    ir_action = [send_command + "fan_off"] * 3
+    multi_command_delay = "-g 20000"        
+    cmd = [ir_ctl, multi_command_delay] + ir_action
+    
+    subprocess.call(cmd)
+    print("performed command: " + str(cmd))
+
+def do_fan_low():
+    ir_action = [send_command + "fan_low"] * 3
+    multi_command_delay = "-g 20000"        
+    cmd = [ir_ctl, multi_command_delay] + ir_action
+    
+    subprocess.call(cmd)
+    print("performed command: " + str(cmd))
+
+def do_fan_medium():
+    ir_action = [send_command + "fan_medium"] * 3
+    multi_command_delay = "-g 20000"        
+    cmd = [ir_ctl, multi_command_delay] + ir_action
+    
+    subprocess.call(cmd)
+    print("performed command: " + str(cmd))
+
+def do_fan_high():
+    ir_action = [send_command + "fan_high"] * 3
+    multi_command_delay = "-g 20000"        
+    cmd = [ir_ctl, multi_command_delay] + ir_action
+    
+    subprocess.call(cmd)
+    print("performed command: " + str(cmd))
+
 
 if __name__ == '__main__':
     port = 4000
