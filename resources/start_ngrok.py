642184f830930f7ad8655ce54ce34f8d1061f4ea
haarismemon <memonhaaris97@gmail.com>
2020-04-02 23:58:35 +0100

    Update ngrok script to point to europe server

--- resources/start_ngrok.py
+++ resources/start_ngrok.py
@@ -2,5 +2,4 @@
 
 from subprocess import call
 
-call(["./ngrok", "authtoken", "1Yzb7iEErkPjUT6z6Rk0XWmzfOB_4xM8nqQa1LcQ4gvCBrR3f"])
-call(["./ngrok", "http", "4000"])
+call(["./ngrok", "http", "4000", "--region=eu"])
