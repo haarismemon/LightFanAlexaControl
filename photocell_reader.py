311e4315feb4b876f1ed7318da9f47adc7aeff9d
haarismemon <memonhaaris97@gmail.com>
2020-07-01 15:51:37 +0100

    Reduce reading for light to be on
    Replaced the 10 uF capacitor with a 1uF capacitor in the photocell circuit.
    That way it takes much less time to fill up, and quicker to recognise whether the light is on.

--- photocell_reader.py
+++ photocell_reader.py
@@ -3,7 +3,7 @@ import time
 
 GPIO.setmode(GPIO.BOARD)
 
-light_on_reading = 5000
+light_on_reading = 350
 
 def is_light_on():
     reading = photocell_reading(light_on_reading)
@@ -13,20 +13,20 @@ def is_light_on():
 
 def photocell_reading(limit):
     count = 0
-    
+
     #define the pin that goes to the circuit
     pin_to_circuit = 16
 
-    #Output on the pin for 
+    #Output on the pin for
     GPIO.setup(pin_to_circuit, GPIO.OUT)
     GPIO.output(pin_to_circuit, GPIO.LOW)
     time.sleep(0.1)
 
     #Change the pin back to input
     GPIO.setup(pin_to_circuit, GPIO.IN)
-  
+
     #Count until the pin goes high
     while (GPIO.input(pin_to_circuit) == GPIO.LOW) and count != (limit + 1):
         count += 1
-        
+
     return count
